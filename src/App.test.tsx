ab1d2af68aefdeb8b88ecfd7048c9767f7b5201c
centsent <wanzau@proton.me>
2022-05-12 14:40:56 +0800

    refactor: remove redux

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
+    <React.StrictMode>
       <App />
-    </Provider>
+    </React.StrictMode>
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
